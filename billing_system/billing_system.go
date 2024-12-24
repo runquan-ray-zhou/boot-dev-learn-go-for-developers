51f86278809fd39e8533704a7f431847fa912c04
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-24 17:30:23 -0500

    complete solution

--- billing_system/billing_system.go
+++ billing_system/billing_system.go
@@ -25,10 +25,21 @@ package main
 
 func calculateFinalBill(costPerMessage float64, numMessages int) float64 {
 	// ?
+	baseBill := calculateBaseBill(costPerMessage, numMessages)
+	discountRate := calculateDiscountRate(numMessages)
+	discountAmount := baseBill * discountRate
+	return baseBill - discountAmount
 }
 
 func calculateDiscountRate(messagesSent int) float64 {
 	// ?
+	if messagesSent > 5000 {
+		return 0.20
+	} else if messagesSent > 1000 {
+		return 0.10
+	} else {
+		return 0.0
+	}
 }
 
 // don't touch below this line
@@ -36,3 +47,33 @@ func calculateDiscountRate(messagesSent int) float64 {
 func calculateBaseBill(costPerMessage float64, messagesSent int) float64 {
 	return costPerMessage * float64(messagesSent)
 }
+
+/*
+Boot.dev
+
+package main
+
+func calculateFinalBill(costPerMessage float64, numMessages int) float64 {
+	baseBill := calculateBaseBill(costPerMessage, numMessages)
+	discountPercentage := calculateDiscountRate(numMessages)
+	discountAmount := baseBill * discountPercentage
+	finalBill := baseBill - discountAmount
+	return finalBill
+}
+
+func calculateDiscountRate(messagesSent int) float64 {
+	if messagesSent > 5000 {
+		return 0.20
+	}
+	if messagesSent > 1000 {
+		return 0.10
+	}
+	return 0.0
+}
+
+// don't touch below this line
+
+func calculateBaseBill(costPerMessage float64, messagesSent int) float64 {
+	return costPerMessage * float64(messagesSent)
+}
+*/
